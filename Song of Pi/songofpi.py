7b17d76bc39806d7f9f57ec0132599cc3b8ffbd2
Javis Sullivan <JavisSullivan@gmail.com>
2015-05-18 13:58:59 -0400

    ...really?

--- Song of Pi/songofpi.py
+++ Song of Pi/songofpi.py
@@ -9,6 +9,6 @@ def PI_Song(string):
 #end
 if __name__ == '__main__':
     PI = "31415926535897932384626433833"
-    for i in range(int(input()):
+    for i in range(int(input())):
         print(PI_Song(input()))
         
