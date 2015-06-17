52299591714bb4a4ee7a3b45a0df5717869d2ca9
Javis Sullivan <JavisSullivan@gmail.com>
2015-06-17 14:29:58 -0400

    Starting on almost sorted challenge

---
+++ Sorting/almost_sorted.py
@@ -0,0 +1,16 @@
+
+def swap(index1, index2):
+	pass
+
+def reverse(index1, index2):
+	pass
+
+def almost_sorted(array):
+	pass
+#fin
+
+if __name__ == '__main__':
+	size = int(input())
+
+	# TODO convert this to a generator for practice
+	array = [int(i) for i in input().split()]
---
+++ Sorting/test_almost_sorted.py
@@ -0,0 +1,16 @@
+import unittest
+from almost_sorted import *
+
+class TestAlmost_Sorted(unittest.TestCase):
+
+	def test_swap_function(self):
+		pass
+
+	def test_reverse_function(self):
+		pass
+
+	def test_almost_sorted_results(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
