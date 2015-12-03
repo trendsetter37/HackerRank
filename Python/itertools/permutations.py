b53e67b86ed016d6c9f220aefbae5853b7d8639c
Javis Sullivan <javissullivan@gmail.com>
2015-12-03 10:00:30 -0500

    with replacement added

--- Python/itertools/permutations.py
+++ Python/itertools/permutations.py
@@ -1,26 +1,34 @@
-''' Your task is to print all possible permutations
+'''
+    For HackerRank Python Section
+    Your task is to print all possible permutations
     of size k of the string in lexicographic sorted order.
 '''
-from itertools import permutations, combinations
+from itertools import permutations, combinations, combinations_with_replacement
 
 
-def hacker_output(lis):
+def _hacker_output(lis):
     return '\n'.join(map(lambda tup: ''.join(tup), lis))
 
 
+def _split_vars(string):
+    string, length = string.split(' ')
+    return ''.join(sorted(list(string))), int(length)
+
+
 def permute(input_string):
-    string, length = input_string.split(' ')
-    length = int(length)
-    string = ''.join(sorted(list(string)))
+    string, length = _split_vars(input_string)
     result = list(permutations(string, length))
-    return hacker_output(result)
+    return _hacker_output(result)
 
 
 def combinate(input_string):
-    string, length = input_string.split(' ')
-    length = int(length)
-    string = ''.join(sorted(list(string)))
+    string, length = _split_vars(input_string)
     result = []
     for i in xrange(1, length + 1):
         result.extend(list(combinations(string, i)))
-    return hacker_output(result)
+    return _hacker_output(result)
+
+
+def combinate_with_replace(input_string):
+    string, length = _split_vars(input_string)
+    return _hacker_output(list(combinations_with_replacement(string, length)))
--- Python/itertools/test_permutations.py
+++ Python/itertools/test_permutations.py
@@ -1,5 +1,5 @@
 import unittest
-from permutations import permute, combinate
+from permutations import permute, combinate, combinate_with_replace
 
 
 class TestPermutations(unittest.TestCase):
@@ -8,6 +8,7 @@ class TestPermutations(unittest.TestCase):
         self.sample_output = 'AC\nAH\nAK\nCA\nCH\nCK\nHA\n' + \
             'HC\nHK\nKA\nKC\nKH'
         self.sample_output_combin = 'A\nC\nH\nK\nAC\nAH\nAK\nCH\nCK\nHK'
+        self.replacement_output = 'AA\nAC\nAH\nAK\nCC\nCH\nCK\nHH\nHK\nKK'
 
     def test_permute_function(self):
         self.assertEqual(
@@ -19,6 +20,12 @@ class TestPermutations(unittest.TestCase):
         self.assertEqual(
             combinate(self.sample_input),
             self.sample_output_combin
+        )
+
+    def test_combinate_with_replacement(self):
+        self.assertEqual(
+            combinate_with_replace(self.sample_input),
+            self.replacement_output
         )
 
 if __name__ == '__main__':
