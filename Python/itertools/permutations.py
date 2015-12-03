e6ad65f2adaafa86878e3bef2ac6bff4e5adb193
Javis Sullivan <javissullivan@gmail.com>
2015-12-03 09:40:11 -0500

    Added combinations

--- Python/itertools/permutations.py
+++ Python/itertools/permutations.py
@@ -1,12 +1,26 @@
 ''' Your task is to print all possible permutations
     of size k of the string in lexicographic sorted order.
 '''
-from itertools import permutations
+from itertools import permutations, combinations
+
+
+def hacker_output(lis):
+    return '\n'.join(map(lambda tup: ''.join(tup), lis))
 
 
 def permute(input_string):
     string, length = input_string.split(' ')
+    length = int(length)
     string = ''.join(sorted(list(string)))
-    return '\n'.join(
-        map(lambda tup: ''.join(tup), list(permutations(string, int(length))))
-    )
+    result = list(permutations(string, length))
+    return hacker_output(result)
+
+
+def combinate(input_string):
+    string, length = input_string.split(' ')
+    length = int(length)
+    string = ''.join(sorted(list(string)))
+    result = []
+    for i in xrange(1, length + 1):
+        result.extend(list(combinations(string, i)))
+    return hacker_output(result)
--- Python/itertools/test_permutations.py
+++ Python/itertools/test_permutations.py
@@ -1,5 +1,5 @@
 import unittest
-from permutations import permute
+from permutations import permute, combinate
 
 
 class TestPermutations(unittest.TestCase):
@@ -7,11 +7,18 @@ class TestPermutations(unittest.TestCase):
         self.sample_input = 'HACK 2'
         self.sample_output = 'AC\nAH\nAK\nCA\nCH\nCK\nHA\n' + \
             'HC\nHK\nKA\nKC\nKH'
+        self.sample_output_combin = 'A\nC\nH\nK\nAC\nAH\nAK\nCH\nCK\nHK'
 
     def test_permute_function(self):
         self.assertEqual(
             permute(self.sample_input),
             self.sample_output
+        )
+
+    def test_combinate_function(self):
+        self.assertEqual(
+            combinate(self.sample_input),
+            self.sample_output_combin
         )
 
 if __name__ == '__main__':
