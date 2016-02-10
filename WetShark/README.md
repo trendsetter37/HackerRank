d8b8f832c04246b3788babc08c0b7f74bad5f350
Javis Sullivan <javissullivan@gmail.com>
2016-02-10 15:11:04 -0500

    Wetshark stuff

---
+++ WetShark/README.md
@@ -0,0 +1,3 @@
+### Wetshark and subsequences
+---
+Visit problem set [here](https://www.hackerrank.com/challenges/wet-shark-and-two-subsequences)
---
+++ WetShark/test/wetshark_test.py
@@ -0,0 +1,8 @@
+import unittest
+import wetshark_test
+
+class WetSharkTestCase(unittest.TestCase):
+    def setUp(self):
+        self.first_testcase = ('4 5 3', '1 1 1 4')
+
+    
