2779c8e60691fe6947c0221fd659c0cb421a7c2e
Javis Sullivan <javissullivan@gmail.com>
2016-02-10 16:36:18 -0500

    getting structure

---
+++ WetShark/wetshark.py
@@ -0,0 +1,8 @@
+
+def dryshark(constraints, array):
+    m, r, s = map(int, constraints.split())
+    arr = map(int, array.split())
+    
+
+if __name__ == '__main__':
+    print(dryshark('4 5 3', '1 1 1 4'))
